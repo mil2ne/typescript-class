baa7b1d949428fce04aee2f9b882ee52ae2b2c15
mil2ne <show3070@gmail.com>
2022-02-25 11:15:39 +0900

    typescript class-2 (constructor, initialize)

--- example.js
+++ example.js
@@ -1,8 +1,17 @@
 "use strict";
 class Person {
-    constructor(name) {
-        this.name = name;
+    constructor(age) {
+        this.name = "Mark";
+        if (age === undefined) {
+            this.age = 20;
+        }
+        else {
+            this.age = age;
+        }
     }
 }
-const p1 = new Person("Mark");
+const p1 = new Person(22);
+const p2 = new Person();
 console.log(p1);
+console.log(p1.age);
+console.log(p2);
--- example.ts
+++ example.ts
@@ -1,10 +1,19 @@
 class Person {
-  name;
-  constructor(name: string) {
-    this.name = name;
+  name: string = "Mark";
+  age: number;
+
+  constructor(age?: number){
+    if(age === undefined){
+      this.age = 20;
+    } else {
+      this.age = age;
+    }
   }
 }
 
-const p1 = new Person("Mark");
+const p1 = new Person(22);
+const p2 = new Person();
 
 console.log(p1);
+console.log(p1.age);
+console.log(p2);
