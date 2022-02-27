2efe59fcc0a2582354e27d305ba150f5bd2a62da
mil2ne <show3070@gmail.com>
2022-02-28 06:58:32 +0900

    typescript class-5 getter, setter

--- example.js
+++ example.js
@@ -1,9 +1,19 @@
 "use strict";
 class Person {
-    constructor(name, age) {
-        this.name = name;
+    constructor(_name, age) {
+        this._name = _name;
         this.age = age;
+    }
+    get name() {
+        // console.log('get');
+        return this._name + ' Hello';
+    }
+    set name(n) {
+        console.log('set');
+        this._name = n;
     }
 }
 const p1 = new Person("Mark", 22);
-console.log(p1);
+console.log(p1.name); // get
+p1.name = "David"; //  set
+console.log(p1.name);
--- example.ts
+++ example.ts
@@ -1,8 +1,17 @@
 class Person {
-  public constructor(public name: string, private age: number){
+  public constructor(private _name: string, private age: number){}
+
+  get name() {
+    return this._name + ' Hello';
+  }
+
+  set name(n: string) {
+    this._name = n;
   }
 }
 
 
 const p1 = new Person("Mark",22);
-console.log(p1);
+console.log(p1.name); // get
+p1.name = "David" //  set
+console.log(p1.name);
