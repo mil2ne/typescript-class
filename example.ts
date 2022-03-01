9c3d33e8165fcebc9f653b17fd45890b493e0320
mil2ne <show3070@gmail.com>
2022-03-02 06:55:29 +0900

    typescript class-11 abstract

--- example.ts
+++ example.ts
@@ -1,27 +1,16 @@
-class Parent {
-  constructor(protected _name: string, private _age: number) {}
+abstract class AbstractPerson {
+  protected _name: string = 'Mark';
 
-  public print(): void {
-    console.log(`이름은 ${this._name} 이고 나이는 ${this._age} 입니다.`)
-  }
-
-  protected printName(): void {
-    console.log(this._name, this._age);
-  }
+  abstract setName(name: string): void;
 }
 
-const p = new Parent('Mark', 39);
-p.print();
-
-class Child extends Parent {
-  public gender = "male";
-
-  constructor(age: number) {
-    super('Mark Jr', age);
-    this.printName();
-
+class Person extends AbstractPerson {
+  setName(name: string): void {
+    this._name = name;
   }
+
 }
 
-const c = new Child(1);
-c.print();
+const p = new Person();
+p.setName('Mark Jr');
+
